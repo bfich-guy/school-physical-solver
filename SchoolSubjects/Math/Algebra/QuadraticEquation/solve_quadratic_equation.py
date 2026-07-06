90a4a50baa50aecb9f97feab0aeeea356ed9b118
BFich <budagyanvahe@gmail.com>
2026-07-06 14:15:32 +0400

    git commit --amend -m "chore(SchoolSubjects/Math/Algebra): rebuild architecture using Strategy pattern" -m "New architectural pattern of the project: Strategy. It means that functions calculating a specific physics or mathematics concept in different ways are located in a single file dedicated to that concept."

--- SchoolSubjects/Math/Algebra/QuadraticEquation/solve_quadratic_equation.py
+++ SchoolSubjects/Math/Algebra/QuadraticEquation/solve_quadratic_equation.py
@@ -16,3 +16,19 @@ def solve_quadratic_equation_by_discriminant(
     root_list: list[Decimal] = [root_1, root_2]
     return root_list
 
+def get_sum_and_product_of_quadratic_equation_roots_by_viet_theorem(
+    *,
+    quadratic_coefficient: Decimal,
+    linear_coefficient: Decimal,
+    constant_term: Decimal,
+) -> list[Decimal]:
+
+    new_linear_coefficient: Decimal = linear_coefficient / quadratic_coefficient
+    new_constant_term: Decimal = constant_term / quadratic_coefficient
+
+    root_sum: Decimal = -new_linear_coefficient
+    root_product: Decimal = new_constant_term
+
+    viet_theorem_list: list[Decimal] = [root_sum, root_product]
+    return viet_theorem_list
+
